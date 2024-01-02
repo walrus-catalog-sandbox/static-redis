c04eaa9bc5f385dc93d13f0dff87826a989d629e
thxCode <thxcode0824@gmail.com>
2024-01-02 11:20:13 +0800

    refactor: remove endpoints output
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -56,11 +56,3 @@ output "port" {
 output "password" {
   value = nonsensitive(module.this.password)
 }
-
-output "endpoints" {
-  value = module.this.endpoints
-}
-
-output "endpoints_readonly" {
-  value = module.this.endpoints_readonly
-}
